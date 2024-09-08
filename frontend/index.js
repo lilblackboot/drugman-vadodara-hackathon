0cfc66d1cc70b91f62ff627aae6ad10f2ea8b246
lilblackboot <suzanmansuri0000@gmail.com>
2024-09-08 18:53:39 +0530

    add button added

---
+++ frontend/index.js
@@ -0,0 +1,20 @@
+
+
+
+document.getElementById("addProductButton").addEventListener("click", displayForm);
+
+function displayForm() {
+  document.getElementById("addProductForm").innerHTML = `
+  <form>
+    <div class="mb-3">
+      <label class="form-label">Product Name</label>
+      <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    </div>
+    <div class="mb-3">
+      <label for="exampleInputPassword1" class="form-label">Amount</label>
+      <input type="number" class="form-control" id="exampleInputPassword1">
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
+`;
+}
